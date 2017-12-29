878743d7f39667cdb1e28079c83e0f1889d1f284
Crepin Fadjo <crepin.fadjo@qanbio.com>
2017-12-29 14:22:34 +0100

    display item when click it in the list

--- src/app/ideas-list/idea-detail/idea-detail.component.ts
+++ src/app/ideas-list/idea-detail/idea-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Idea } from '../../models/idea';
 
 @Component({
   selector: 'app-idea-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./idea-detail.component.css']
 })
 export class IdeaDetailComponent implements OnInit {
+ @Input() idea:Idea;
 
   constructor() { }
 
